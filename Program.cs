8eb471fecf063b164993026ed749cd19acc06d5d
vino04003 <89254939+vino04003@users.noreply.github.com>
2021-11-16 15:52:46 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace okej
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=10, b=20;
+            BytPlats (ref a);
+            BytPlats (ref b);
+            Console.WriteLine(a + " " + b );
+        }
+
+        public static void BytPlats(ref int t)
+        {
+            if(t!=20){
+                t=20;
+            }
+            else if(t!=10){
+                t=10;
+            }
+        }
+    }
+}
